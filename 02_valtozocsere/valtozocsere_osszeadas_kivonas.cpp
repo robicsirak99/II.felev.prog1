90d0d010f72b2346c778de45cfb159689c82ea07
robicsirak99 <36278858+robicsirak99@users.noreply.github.com>
2018-02-15 18:50:14 +0100

    Add files via upload

---
+++ 02_valtozocsere/valtozocsere_osszeadas_kivonas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x=10;
+    int y=20;
+
+    cout << x << " " << y << endl;
+
+    x=y-x;
+    y=y-x;
+    x=y+x;
+
+    cout << x << " " << y;
+
+    return 0;
+}
